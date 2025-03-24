ce75608c8b838356e7aff477a4c8c6d20fac3f79
Ahairwe Jordan <2000809725@std.kyu.ac.ug>
2025-03-24 12:36:10 +0300

    show forecast and actual expenditure on the budget items

--- app/Models/Budget.php
+++ app/Models/Budget.php
@@ -10,26 +10,32 @@ class Budget extends Model
 {
     use HasFactory;
     use SoftDeletes;
-    protected $fillable = ['phase_id','budget_name', 'description', 'estimated_amount', 'milestone_id','approved'];
+    protected $fillable = ['phase_id', 'budget_name', 'description', 'estimated_amount', 'milestone_id', 'approved'];
     protected $table = 'budgets';
-   
-    public function projectPlans()
-{
-    return $this->belongsTo(ProjectPlan::class, 'project_plan_id');
-}
-//expense
-// Budget.php (Model)
-public function phase()
-{
-    return $this->belongsTo(Phase::class);
-}
 
-public function milestone()
-{
-    return $this->belongsTo(Milestone::class);
-}
-public function project()
-{
-    return $this->belongsTo(Project::class,'project_plan_id');
-}
+    public function projectPlans()
+    {
+        return $this->belongsTo(ProjectPlan::class, 'project_plan_id');
+    }
+    //expense
+    // Budget.php (Model)
+    public function phase()
+    {
+        return $this->belongsTo(Phase::class);
+    }
+
+    public function milestone()
+    {
+        return $this->belongsTo(Milestone::class);
+    }
+    public function project()
+    {
+        return $this->belongsTo(Project::class, 'project_plan_id');
+    }
+
+    // budget items
+    public function items()
+    {
+        return $this->hasMany(BudgetItem::class, 'budget_id');
+    }
 }
--- resources/views/livewire/budget/budget-details-component.blade.php
+++ resources/views/livewire/budget/budget-details-component.blade.php
@@ -86,6 +86,32 @@
                                             <th scope="col"
                                                 class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                                 Estimated Amount</th>
+                                            <th scope="col"
+                                                class="relative px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                                                x-data="{ showTooltip: false }">
+                                                Forecast Amount
+                                                <span class="ml-1 text-gray-400 cursor-pointer"
+                                                    @mouseover="showTooltip = true" @mouseleave="showTooltip = false">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                        class="w-4 h-4">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+                                                    </svg>
+                                                </span>
+
+                                                <!-- Tooltip -->
+                                                <div x-show="showTooltip"
+                                                    class="absolute left-0 mt-2 w-64 p-2 text-xs text-white bg-gray-900 rounded-lg shadow-lg"
+                                                    style="display: none;">
+                                                    The Forecast Amount is the total amount if all requested expenses
+                                                    were fully approved.
+                                                </div>
+                                            </th>
+
+                                            <th scope="col"
+                                                class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                Total Spent</th>
 
                                             <th scope="col" class="relative py-3.5 px-4">
                                                 <span class="sr-only">Action</span>
@@ -96,7 +122,8 @@
                                         class="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
                                         @forelse($budgetItems as $item)
                                             <tr>
-                                                <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+                                                <td
+                                                    class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
                                                     <div class="inline-flex items-center gap-x-3">
                                                         <input type="checkbox"
                                                             class="text-blue-500 border-gray-300 rounded dark:bg-gray-900 dark:ring-offset-gray-900 dark:border-gray-700">
@@ -105,7 +132,7 @@
 
                                                             <div>
                                                                 <h2 class="font-medium text-gray-800 dark:text-white ">
-                                                                    {{ $item->expenseCategoryItem?->name ?? "NaN" }}
+                                                                    {{ $item->expenseCategoryItem?->name ?? 'NaN' }}
                                                                 </h2>
                                                                 <p
                                                                     class="text-sm font-normal text-gray-600 dark:text-gray-400">
@@ -125,18 +152,29 @@
                                                 </td>
                                                 <td class="px-4 py-4 text-sm whitespace-nowrap">
                                                     <div class="flex items-center gap-x-2">
-                                                        {{ number_format($item->estimated_amount, 0, ".", ",") }}
+                                                        {{ number_format($item->estimated_amount, 0, '.', ',') }}
+                                                    </div>
+                                                </td>
+                                                <td class="px-4 py-4 text-sm whitespace-nowrap">
+                                                    <div class="flex items-center gap-x-2">
+                                                        {{ number_format($item->expenses_sum_amount_paid ?? 0, 0, '.', ',') }}
+                                                    </div>
+                                                </td>
+                                                <td class="px-4 py-4 text-sm whitespace-nowrap">
+                                                    <div class="flex items-center gap-x-2">
+                                                        {{ number_format($item->approved_expenses_sum_amount_paid ?? 0, 0, '.', ',') }}
                                                     </div>
                                                 </td>
                                                 <td class="px-4 py-4 text-sm whitespace-nowrap">
                                                     <div class="flex items-center gap-x-4">
                                                         <!-- Edit Button -->
-                                                      
-                                                        <button wire:click="openEditBudgetItemModal({{ $item->id }})"
+
+                                                        <button
+                                                            wire:click="openEditBudgetItemModal({{ $item->id }})"
                                                             class="bg-blue-500 text-white px-4 py-2 rounded-lg transition-colors duration-200 hover:bg-blue-600 focus:outline-none flex items-center gap-2">
                                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                                class="w-5 h-5">
+                                                                viewBox="0 0 24 24" stroke-width="1.5"
+                                                                stroke="currentColor" class="w-5 h-5">
                                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                                     d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                                                             </svg>
@@ -144,11 +182,12 @@
                                                         </button>
 
                                                         <!-- Delete Button -->
-                                                        <button wire:click="openDeleteBudgetItemModal({{ $item->id }})"
+                                                        <button
+                                                            wire:click="openDeleteBudgetItemModal({{ $item->id }})"
                                                             class="bg-red-500 text-white px-4 py-2 rounded-lg transition-colors duration-200 hover:bg-red-600 focus:outline-none flex items-center gap-2">
                                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                                class="w-5 h-5">
+                                                                viewBox="0 0 24 24" stroke-width="1.5"
+                                                                stroke="currentColor" class="w-5 h-5">
                                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                                     d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                                             </svg>
@@ -161,7 +200,8 @@
                                             </tr>
                                         @empty
                                             <tr>
-                                                <td colspan="6" class="text-center py-4 text-gray-500 dark:text-gray-300">No
+                                                <td colspan="6"
+                                                    class="text-center py-4 text-gray-500 dark:text-gray-300">No
                                                     budget items found</td>
                                             </tr>
                                         @endforelse
@@ -178,8 +218,8 @@
                     <!-- Previous Button -->
                     <button wire:click="previousPage"
                         class="flex items-center px-5 py-2 text-sm font-medium text-gray-700 capitalize transition-colors duration-200 bg-white border border-gray-300 rounded-lg gap-x-2 hover:bg-gray-100 dark:bg-gray-800 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-700">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-5 h-5 rtl:-scale-x-100">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-5 h-5 rtl:-scale-x-100">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
                         </svg>
@@ -203,8 +243,8 @@
                     <button wire:click="nextPage"
                         class="flex items-center px-5 py-2 text-sm font-medium text-gray-700 capitalize transition-colors duration-200 bg-white border border-gray-300 rounded-lg gap-x-2 hover:bg-gray-100 dark:bg-gray-800 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-700">
                         <span>Next</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-5 h-5 rtl:-scale-x-100">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-5 h-5 rtl:-scale-x-100">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
                         </svg>
@@ -226,8 +266,9 @@
                                 Category item <span class="text-red-500">*</span></label>
                             <select wire:model="selectedExpenseCategory" id="expense_category"
                                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 bg-white py-2 px-3 text-gray-700">
-                                <option value="" selected class="text-gray-400">--Select an expense category--</option>
-                                @foreach($expenseCategoryItems as $categoryitem)
+                                <option value="" selected class="text-gray-400">--Select an expense category--
+                                </option>
+                                @foreach ($expenseCategoryItems as $categoryitem)
                                     <option value="{{ $categoryitem->id }}">{{ $categoryitem->name }}</option>
                                 @endforeach
                             </select>
@@ -281,8 +322,9 @@
                                 Category item <span class="text-red-500">*</span></label>
                             <select wire:model="selectedEditExpenseCategory" id="edit_expense_category"
                                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 bg-white py-2 px-3 text-gray-700">
-                                <option value="" selected class="text-gray-400">--Select an expense category--</option>
-                                @foreach($expenseCategoryItems as $categoryitem)
+                                <option value="" selected class="text-gray-400">--Select an expense category--
+                                </option>
+                                @foreach ($expenseCategoryItems as $categoryitem)
                                     <option value="{{ $categoryitem->id }}">{{ $categoryitem->name }}</option>
                                 @endforeach
                             </select>
@@ -321,10 +363,10 @@
             <x-dialog-modal wire:model="showDeleteBudgetItemModal">
                 <x-slot name="title">Delete Budget Item</x-slot>
                 <x-slot name="content">
-                {{-- confirmation --}}
+                    {{-- confirmation --}}
                     <p>Are you sure you want to delete this budget item?</p>
                     <p>This action cannot be undone.</p>
-                    
+
                 </x-slot>
                 <x-slot name="footer">
                     <button wire:click="closeDeleteBudgetItemModal"
